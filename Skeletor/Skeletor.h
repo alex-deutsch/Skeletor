b8146e7658c282998b6f54613c8a1f930f3c8a5f
Alexander Deutsch <deutscha@mediamarktsaturn.com>
2018-08-17 14:17:43 +0200

    Initial Commit

---
+++ Skeletor/Skeletor.h
@@ -0,0 +1,19 @@
+//
+//  Skeletor.h
+//  Skeletor
+//
+//  Created by Alexander Deutsch on 17.08.18.
+//  Copyright © 2018 Alexander Deutsch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Skeletor.
+FOUNDATION_EXPORT double SkeletorVersionNumber;
+
+//! Project version string for Skeletor.
+FOUNDATION_EXPORT const unsigned char SkeletorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Skeletor/PublicHeader.h>
+
+
